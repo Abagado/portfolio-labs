686c004605490d4bcb6946a7607429ba1d025e94
Abagado <147521764+Abagado@users.noreply.github.com>
2025-01-20 14:53:19 +1000

    Add Lab 1 implementation

---
+++ lab1/src/pages/Home.tsx
@@ -0,0 +1,18 @@
+import Introduction from '../components/Introduction';
+import ProfilePic from '../components/ProfilePic';
+
+
+const Home = () => {
+  return (
+    <div className="ml-[90px] min-h-screen grid grid-cols-1 md:grid-cols-3">
+      <div className="col-span-2 p-5">
+        <Introduction />
+      </div>
+      <div className="hidden md:block">
+        <ProfilePic />
+      </div>
+    </div>
+  );
+};
+
+export default Home;
